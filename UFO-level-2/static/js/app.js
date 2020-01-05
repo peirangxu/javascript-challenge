d8f19d0b10624746149acc2b62ca1c4819b9e92f
Peirang Xu <tennyxu1995@gmail.com>
2020-01-05 14:38:44 -0800

    update level2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -5,8 +5,13 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Input element and button
-var input_element = d3.select(".form-control");
-var button = d3.selectAll(".dropdown-item");
+var datetime = d3.select("#datetime");
+var city = d3.select("#city");
+var state = d3.select("#state");
+var country = d3.select("#country");
+var shape = d3.select("#shape");
+
+var button = d3.select("#filter-btn");
 
 // Append table to webpage
 function table(r){
@@ -23,19 +28,43 @@ table(tableData);
 function handleChange() {
     tbody.html("");
     // Select input value
-    input_text = input_element.property("value").toLowerCase();
-    filter_category = d3.select(this).text();
-    console.log(filter_category);
-    console.log(input_text);
+    var dateInputText = datetime.property("value").toLowerCase();
+    var cityInputText = city.property("value").toLowerCase();
+    var stateInputText = state.property("value").toLowerCase();
+    var countryInputText = country.property("value").toLowerCase();
+    var shapeInputText = shape.property("value").toLowerCase();
 
+    console.log(dateInputText);
+    console.log(cityInputText);
+    console.log(stateInputText);
+    console.log(countryInputText);
+    console.log(shapeInputText);
+
+    filter_table = tableData
     // Filtered data from tableData
-    filtered_data = tableData.filter(tableData => tableData[filter_category] === input_text);
-    if (filtered_data.length !== 0) {
-        table(filtered_data);
-    } else {
-        alert("Result Not Found");
-        table(tableData);
-    };
+    if(dateInputText !== ""){
+        var filter_table = filter_table.filter(filter_table => filter_table.datetime === dateInputText);
+    } 
+    if(cityInputText !== ""){
+        var filter_table = filter_table.filter(filter_table => filter_table.city === cityInputText);
+    }
+    if(stateInputText !== ""){
+        var filter_table = filter_table.filter(filter_table => filter_table.state === stateInputText);
+    }
+    if(countryInputText !== ""){
+        var filter_table = filter_table.filter(filter_table => filter_table.country === countryInputText);
+    }
+    if(shapeInputText !== ""){
+        var filter_table = filter_table.filter(filter_table => filter_table.shape === shapeInputText);
+    }
 
+    console.log(filter_table.length);
+    
+    if (filter_table.length !== 0){
+        table(filter_table);
+    } else{
+        alert("Results Not Found");
+        table(tableData);
+    }
 }
 button.on("click", handleChange)
